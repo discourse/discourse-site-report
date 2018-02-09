bdf903c8c7d1f659b6ac1ced6e92ae682988f5ea
scossar <scossar3@gmail.com>
2018-02-08 16:37:50 -0800

    Use Discourse Report module

--- app/mailers/site_report_mailer.rb
+++ app/mailers/site_report_mailer.rb
@@ -1,3 +1,5 @@
+require_dependency 'report'
+
 class SiteReportMailer < ActionMailer::Base
   include Rails.application.routes.url_helpers
 
@@ -5,6 +7,12 @@ class SiteReportMailer < ActionMailer::Base
   default from: SiteSetting.notification_email
   def report
     subject =  'this is a test'
+    start_date = 1.month.ago.beginning_of_month
+    end_date = 1.month.ago.end_of_month
+    visits_report = Report.find(:visits, start_date: start_date, end_date: end_date)
+
+    puts "REPORTDATACURRENT #{visits_report.total}"
+    puts "REPORTDATAPREV #{visits_report.prev30Days}"
 
     admin_emails = User.where(admin: true).map(&:email).select {|e| e.include?('@')}
 
