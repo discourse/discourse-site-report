e9f4d5b5192d9b03a592b7a35d00c3ada77d4048
scossar <scossar3@gmail.com>
2018-02-09 16:14:29 -0800

    Add helper module

---
+++ app/helpers/site_report_helper.rb
@@ -0,0 +1,86 @@
+module SiteReportHelper
+
+  # todo: the rtl? method needs to be included from ApplicationHelper
+  def dir_for_locale
+    rtl? ? 'rtl' : 'ltr'
+  end
+
+  def logo_url
+    logo_url = SiteSetting.digest_logo_url
+    logo_url = SiteSetting.logo_url if logo_url.blank? || logo_url =~ /\.svg$/i
+
+    return nil if logo_url.blank? || logo_url =~ /\.svg$/i
+    if logo_url !~ /http(s)?\:\/\//
+      logo_url = "#{Discourse.base_url}#{logo_url}"
+    end
+
+    logo_url
+  end
+
+  def header_color
+    "##{ColorScheme.hex_for_name('header_primary')}"
+  end
+
+  def header_bgcolor
+    "##{ColorScheme.hex_for_name('header_background')}"
+  end
+
+  def anchor_color
+    "##{ColorScheme.hex_for_name('tertiary')}"
+  end
+
+  def bg_color
+    '#eeeeee'
+  end
+
+  def text_color
+    '#222222'
+  end
+
+  def highlight_bgcolor
+    '#2F70AC'
+  end
+
+  def highlight_color
+    '#ffffff'
+  end
+
+  def body_bgcolor
+    '#ffffff'
+  end
+
+  def body_color
+    '#222222'
+  end
+
+  def report_date(months_ago)
+    months_ago.month.ago.strftime('%B %Y')
+  end
+
+  def site_report_title(months_ago)
+    "#{I18n.t('statistics_digest.title')} #{report_date(months_ago)}"
+  end
+
+  def spacer_color(outer_count, inner_count = 0)
+    outer_count == 0 && inner_count == 0 ? highlight_bgcolor : bg_color
+  end
+
+  def table_border_style(total_rows, current_row)
+    unless total_rows - 1 == current_row
+      "border-bottom:1px solid #dddddd;"
+    end
+  end
+
+  def site_link(color)
+    "<a style='text-decoration:none;color:#{color}' href='#{Discourse.base_url}' style='color: #{color}'>#{SiteSetting.title}</a>"
+  end
+
+  def statistics_digest_link(color)
+    "<a style='text-decoration:none;color:#{color}' href='#{Discourse.base_url}/admin/plugins/admin-statistics-digest' style='color: #{color}'>#{t 'statistics_digest.here'}</a>"
+  end
+
+  def superscript(count)
+    "<sup style='line-height:0;font-size:70%;vertical-align:top;mso-text-raise:50%'>[#{count}]</sup>"
+  end
+
+end
--- app/mailers/site_report_mailer.rb
+++ app/mailers/site_report_mailer.rb
@@ -1,8 +1,12 @@
 require_dependency 'report'
+require_relative '../helpers/site_report_helper'
 
 class SiteReportMailer < ActionMailer::Base
   include Rails.application.routes.url_helpers
-
+  include ApplicationHelper
+  include SiteReportHelper
+  helper :application
+  add_template_helper SiteReportHelper
   append_view_path Rails.root.join('plugins', 'discourse-site-report', 'app', 'views')
   default from: SiteSetting.notification_email
 
@@ -12,19 +16,6 @@ class SiteReportMailer < ActionMailer::Base
     end_date = 1.month.ago.end_of_month
     previous_start_date = 2.months.ago.beginning_of_month
     previous_end_date = 2.months.ago.end_of_month
-
-    visits = Report.find(:visits, start_date: start_date, end_date: end_date)
-    mobile_visits = Report.find(:mobile_visits, start_date: start_date, end_date: end_date)
-    signups = Report.find(:signups, start_date: start_date, end_date: end_date)
-    profile_views = Report.find(:profile_views, start_date: start_date, end_date: end_date)
-    topics = Report.find(:topics, start_date: start_date, end_date: end_date)
-    posts = Report.find(:posts, start_date: start_date, end_date: end_date)
-    time_to_first_response = Report.find(:time_to_first_response, start_date: start_date, end_date: end_date)
-    topics_with_no_response = Report.find(:topics_with_no_response, start_date: start_date, end_date: end_date)
-    emails = Report.find(:emails, start_date: start_date, end_date: end_date)
-    flags = Report.find(:flags, start_date: start_date, end_date: end_date)
-    likes = Report.find(:likes, start_date: start_date, end_date: end_date)
-    accepted_solutions = Report.find(:accepted_solutions, start_date: start_date, end_date: end_date)
 
     discourse_reports = {
       visits: Report.find(:visits, start_date: start_date, end_date: end_date),
@@ -44,29 +35,13 @@ class SiteReportMailer < ActionMailer::Base
     @data = {}
 
     discourse_reports.each do |key, discourse_report|
-      @data[key] = create_data(key, discourse_report.total, discourse_report.prev30Days )
+      @data[key] = create_data(discourse_report.total, discourse_report.prev30Days )
     end
 
     repeat_new_users = repeat_new_users start_date, end_date, 2
     previous_repeat_new_users = repeat_new_users previous_start_date, previous_end_date, 2
 
-    # @data = {
-    #   visits: visits_report ? { current: visits_report.total, prev: visits_report.prev30Days } : nil,
-    #   mobile_visits: mobile_visits_report.total,
-    #   mobile_visits_prev: mobile_visits_report.prev30Days,
-    #   signups: signups_report.total,
-    #   signups_prev: signups_report.prev30Days,
-    #   profile_views: profile_views_report.total,
-    #
-    #   topics: topics_report.total,
-    #   posts: posts_report.total,
-    #   response_time: time_to_first_response_report.total,
-    #   no_response: topics_with_no_response_report.total,
-    #   emails: emails_report.total,
-    #   flags: flags_report.total,
-    #   likes: likes_report.total,
-    #   solutions: accepted_solutions_report.total
-    # }
+    @data[:repeat_new_users] = create_data(repeat_new_users, previous_repeat_new_users)
 
     admin_emails = User.where(admin: true).map(&:email).select {|e| e.include?('@')}
     mail(to: admin_emails, subject: subject)
